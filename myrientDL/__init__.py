a4b5aead4f3b268afc4ac72f23bdb5e0927114c7
JMit-dev <darkkid8192004@gmail.com>
2026-02-27 17:19:33 -0500

    feat: export new collection and verification classes

--- myrientDL/__init__.py
+++ myrientDL/__init__.py
@@ -9,20 +9,35 @@ __version__ = "0.1.0"
 __author__ = "Jordan Mitacek"
 
 from .config import MyrientConfig
-from .models import GameFile, DownloadStatus, DownloadStats
+from .models import (
+    GameFile,
+    DownloadStatus,
+    DownloadStats,
+    Collection,
+    FileFormat,
+    CollectionInfo,
+    DownloadWarning,
+)
 from .database import Database
 from .crawler import MyrientCrawler
 from .downloader import DownloadManager
 from .search import GameSearch, SearchResult
+from .verification import TorrentZipVerifier, ChecksumVerifier
 
 __all__ = [
     "MyrientConfig",
-    "GameFile", 
+    "GameFile",
     "DownloadStatus",
-    "DownloadStats", 
+    "DownloadStats",
+    "Collection",
+    "FileFormat",
+    "CollectionInfo",
+    "DownloadWarning",
     "Database",
     "MyrientCrawler",
     "DownloadManager",
     "GameSearch",
     "SearchResult",
+    "TorrentZipVerifier",
+    "ChecksumVerifier",
 ]
